a0e4ab187cdf03891c317f3d916f581606b26e05
DivyaJain88 <70259096+DivyaJain88@users.noreply.github.com>
2020-09-07 15:33:46 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -26,7 +26,7 @@ gameState = "serve";
 
 function draw() {  
   //fill the computer screen with white color
-  background("white");
+  background("green");
   edges = createEdgeSprites();
   //display Scores
   text(computerScore,170,20);
